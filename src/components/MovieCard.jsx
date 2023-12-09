16686d20a0f58b33bef0b036ba9451885fa2c91a
Subham Mani <manisubham09@gmail.com>
2023-12-09 12:00:04 +0530

    update movie card and movie trailer

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,8 @@ import { MOVIE_POSTER_URL } from "../utils/constants";
 
 const MovieCard = ({ posterPath }) => {
   return (
-    <div className="bg-black">
-      <div className="w-[200px] rounded-lg overflow-hidden cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
+    <div className="bg-black rounded-lg overflow-hidden hover:scale-105 transition ease-in-out duration-300">
+      <div className="w-[200px] cursor-pointer hover:opacity-80 transition ease-in-out duration-300">
         <img
           className="w-full"
           src={MOVIE_POSTER_URL + posterPath}
